094c7ec8f4c7ef4b4a8aea22b5f04e72a892e984
Wimukthi Indeewara <wind3907>
2022-07-07 15:54:25 +0530

    text

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,9 @@
 pipeline {
-    agent { label 'master' }
+    agent any
+    parameters {
+      string(name: 'PLANET', defaultValue: 'Earth', description: 'Which planet are we on?')
+      string(name: 'GREETING', defaultValue: 'Hello', description: 'How shall we greet?')
+    }
     triggers {
         parameterizedCron('''
             # leave spaces where you want them around the parameters. They'll be trimmed.
@@ -9,48 +13,11 @@ pipeline {
         ''')
     }
     stages {
-        stage('Test') {
+        stage('Example') {
             steps {
-                echo 'hi'
-            }
-        }
-        // stage("SWMS Data Migration") {
-        //     steps {
-        //         echo "Section: SWMS Data Migration"
-        //         script {
-        //             try {
-        //                 build job: "swms-db-migrate-AIX-RDS-test", parameters: [
-        //                     string(name: 'SOURCE_DB', value: "${params.SOURCE_DB}"),
-        //                     string(name: 'TARGET_DB', value: "${params.TARGET_DB}"),
-        //                     string(name: 'ROOT_PW', value: ""),
-        //                     string(name: 'TARGET_SERVER', value: "${params.TARGET_SERVER}"),
-        //                     string(name: 'artifact_s3_bucket', value: "${params.artifact_s3_bucket}"),
-        //                     string(name: 'platform', value: "${params.platform}"),
-        //                     string(name: 'artifact_version', value: "${params.artifact_version}"),
-        //                     string(name: 'artifact_name', value: "${params.artifact_name}"),
-        //                     string(name: 'dba_masterfile_names', value: "${params.dba_masterfile_names}"),
-        //                     string(name: 'master_file_retry_count', value: "${params.master_file_retry_count}")
-        //                 ]
-        //                 echo "Data Migration Successful!"
-        //             } catch (e) {
-        //                 echo "Data Migration Failed!"
-        //                 throw e
-        //             }
-        //         }
-        //     }
-        // }  
-    }
-    post {
-        success {
-            script {
-                echo 'Scheduled Data Migration is completed'
-            }
-        }
-        failure {
-            script {
-                echo 'Scheduled Data Migration is Failed'
+                echo "${params.GREETING} ${params.PLANET}"
+                script { currentBuild.description = "${params.GREETING} ${params.PLANET}" }
             }
         }
     }
 }
-
